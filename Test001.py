0a0ee3274ac34e69ac24e32bde9dd357bcc06f4e
sarasnowflake25 <140198571+sarasnowflake25@users.noreply.github.com>
2023-07-21 23:46:51 +0530

    Update Test001.py

--- Test001.py
+++ Test001.py
@@ -1,7 +1,8 @@
 import streamlit
 
-streamlit.title("Welcome to Sara Parents Healthy Dinner")
-streamlit.header("Menu")
-streamlit.title("Dosa")
-streamlit.title("Sapathi")
-            
+streamlit.title('Welcome to Sara Parents Healthy Dinner')
+streamlit.header('Menu')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
