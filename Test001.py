382b9f5df26fcb74dc9f84d1fcad35bbb8b0b3f3
sarasnowflake25 <140198571+sarasnowflake25@users.noreply.github.com>
2023-07-24 22:30:45 +0530

    Update Test001.py

--- Test001.py
+++ Test001.py
@@ -17,3 +17,7 @@ my_fruit_list_show = my_fruit_list.loc[fruit_sel]
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list_show)
 streamlit.dataframe(my_fruit_list)
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
